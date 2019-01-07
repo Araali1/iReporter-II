9bbf83b1e62f87d006090658c89c9abc0ef1b919
Araali1 <nabendeali@gmail.com>
2019-01-07 12:49:56 +0300

    [Started #162996368]Changed Setup and Added blueprint

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import os
-from api import create_app
+from app import create_app
 from instance.config import app_config
 
 config_name = os.getenv('FLASK_ENV')
 app = create_app(app_config['development'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
