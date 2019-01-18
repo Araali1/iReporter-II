82914f48c098a33ae27468bbf17535b52f5a50d0
Araali1 <nabendeali@gmail.com>
2019-01-18 18:17:13 +0300

    [Started #163318230]modified views

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Resource, Api
 
-from app.api.v1.views.viewred import Events, RedFlags, EventDetail
+from app.api.v1.views.views import Events, RedFlags, EventDetail
 
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api=Api(v1)
